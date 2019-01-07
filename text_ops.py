2441afe94b1fb1474ddc06400deb4be1cbc79a5f
Daniel Artemiev <daniel.artemiev@outlook.com>
2019-01-07 22:28:32 +0200

    Daniel
    Text Interface + reading file + output to console

---
+++ text_ops.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+print("Введите имя файла: ")
+filename=input() # choosing file
+try:
+	fdes = open(filename) # opening file 
+	text = fdes.read() # reading
+	fdes.close() #closing
+except FileNotFoundError:
+	print("Файл не найден")
+	exit()
+print(text) # prints all text to console
