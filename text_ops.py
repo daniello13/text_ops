1f3c56f5f55064c6f0607472413af52a161b7b60
SuumCuique <Vlad.V.Kuznetsov@hotmail.com>
2019-01-12 04:32:35 +0200

    success 6 task
    def deviation

--- text_ops.py
+++ text_ops.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
-#changes Viktoriya
+from math import *
+
+def deviation(text):
+	mas = text.split() #разбиваем текст на слова
+	Xsr = i=0 
+	while i < len(mas):
+		Xsr += len(mas[i]) #находим среднее арифметическое длин слов
+		i+=1	
+	Xsr/=len(mas)
+	stdev = i = 0
+	while i<len(mas):
+		stdev += fabs(len(mas[i]) - Xsr) #находим стандартное отклонение
+		i+=1
+	stdev/=len(mas)
+	return stdev
+
+
 print("Введите имя файла: ")
 filename=input() # choosing file
 try:
-	fdes = open(filename) # opening file 
+	fdes = open(filename, 'r', encoding='utf-8') # opening file
 	text = fdes.read() # reading
 	fdes.close() #closing
 except FileNotFoundError:
 	print("Файл не найден")
 	exit()
-print(text) # prints all text to console
+#print(text) # prints all text to console
+print(deviation(text))
+#D:\Downloads\texts\A_Fashion.txt
